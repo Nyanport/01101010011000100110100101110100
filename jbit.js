14bd128d4fddc51653ac75087b6147e3466cf019
dev01111 <105835627+dev01111@users.noreply.github.com>
2022-08-08 14:21:22 +1000

    Update jbit.js

--- jbit.js
+++ jbit.js
@@ -8,7 +8,7 @@ xhttp.onreadystatechange = function() {
         for(let i = 0; i < bitarray.length; i++) {
           var bitfunc = bitarray[i].split(String.fromCharCode(parseInt(0x1CBC)));
           var result = this[ parseInt( jpure.maximise(bitfunc[0]), 2 ) ](bitfunc[1]);
-          if(!result) { console.warn("%cTermination OPCODE ran. OPCODE" + ("000" + (parseInt( jpure.maximise(bitfunc[0]), 2 )).toString(16).toUpperCase()).slice(-4), "font-size: 30pt; font-family: \'Ubuntu\', \'Arial\';"); return; } // Every function must return a result.
+          if(!result) { console.warn("%cTermination OPCODE ran. OPCODE is 0x" + ("000" + (parseInt( jpure.maximise(bitfunc[0]), 2 )).toString(16).toUpperCase()).slice(-4), "font-size: 30pt; font-family: \'Ubuntu\', \'Arial\';"); return; } // Every function must return a result.
         }
       },
       "57005": function() { return false; } // Terminate signal 0xDEAD. Return bit 0 to terminate program
